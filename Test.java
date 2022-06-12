0b2c2ead66cb5c8096d64af4818276c64e79d04b
Pratiksha9296 <106229484+Pratiksha9296@users.noreply.github.com>
2022-06-12 16:21:20 +0530

    Create Test.java

---
+++ Test.java
@@ -0,0 +1,18 @@
+package pack_A;
+
+public class Class1
+{
+	static public int a=10;
+	static int b=20;
+	static private int c=30;
+	static protected int d=40;
+	
+	public static void main(String[] args)
+	{
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println(c);
+		System.out.println(d);
+		}
+}
+  
